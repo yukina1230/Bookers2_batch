161629425f49116ce848e55adda425bede4c60e0
yukina1230 <lost.laugh1230@gmail.com>
2021-04-08 01:22:20 +0000

    [Start]batch

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+  }
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root :to => "homes#top"
+  get "home/about" => "homes#about"
+
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update]
+  resources :users, only: [:index, :show, :edit, :update]
+end
